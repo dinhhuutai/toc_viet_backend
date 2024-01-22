2747c433976d568ea14d829673aa8daf74521f3c
dinhhuutai <dinhhuutai20023107@gmail.com>
2024-01-22 17:47:04 +0700

    upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -11,11 +11,11 @@ const db = require("./config/db");
 // Connect to DB
 db.connect();
 
-const corsOptions ={
-    origin: process.env.CORS_ORIGIN, 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
 
 app.use(cookieParser());
 app.use(express.json());
@@ -23,5 +23,18 @@ app.use(cors(corsOptions));
 routes(app);
 
 const PORT = 5000;
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../frontend/build")));
+
+    app.get("*", (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, "../../", "frontend", "build", "index.html")
+        )
+    );
+} else {
+    app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
